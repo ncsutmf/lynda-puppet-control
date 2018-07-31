bef7f562709d05fbf759bb0f29ed7992ff623e5c
Tom Farwig <tmfarwig@ncsu.edu>
2018-07-31 15:35:25 -0400

    Update Download URL

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,7 +5,7 @@ class minecraft {
   file {'/opt/minecraft/mincraft_server.jar' :
     ensure => file,
 #    source => 'https://launcher.mojang.com/mc/game/1.13/server/d0caafb8438ebd206f99930cfaecfa6c9a13dca0/server.jar',
-    source => 'https://s3.amazonws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
   package {'java':
     ensure => present,
