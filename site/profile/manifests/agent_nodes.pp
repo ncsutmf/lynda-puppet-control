2747da474e4e713a251875959679acc6c90e5546
Tom Farwig <tmfarwig@ncsu.edu>
2018-07-31 14:39:33 -0400

    minetest

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node { 'web.puppet.vm' : }
   dockeragent::node { 'db.puppet.vm' : }
+  dockeragent::node { 'minetest.puppet.vm' : }
   
   host {'web.puppet.vm' :
     ensure => present,
@@ -10,5 +11,9 @@ class profile::agent_nodes {
   host {'db.puppet.vm' :
     ensure => present,
     ip     => '172.18.0.3',
+  }
+  host {'minetest.puppet.vm' :
+    ensure => present,
+    ip     => '172.18.0.4',
   }
 }
